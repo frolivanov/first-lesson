b4bc500417186b677b133f2f7503cd70895c872c
Ледяев Иван <vanechka.pchelkin@bk.ru>
2019-09-18 17:53:07 +0700

    fizbuzz

---
+++ fizbuz.py
@@ -0,0 +1,11 @@
+for i in range(1,1001):
+    if i==666:
+        print ("satan")
+    elif i%3 and i%5 == 0:
+        print ("fizbuz")
+    elif i%5 == 0:
+        print ("buz")
+    elif i%3 == 0:
+        print ("fiz")
+    else:
+        print (i)
