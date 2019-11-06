b97f0cb6a779377390548653639709bd7664baf5
Ледяев Иван <vanechka.pchelkin@bk.ru>
2019-11-06 18:38:00 +0700

    почти доделал книгу

--- интерфейсы/new_kniga.py
+++ интерфейсы/new_kniga.py
@@ -1,12 +1,24 @@
 from tkinter import *
 root=Tk()
 root.title('Книжиенция попытка 2')
+root.geometry('600x600')
 
 
-add_button =Button(root, text='Добавить контакт',background='green')
-del_button =Button(root, text='Удалить контакт')
-edit_button =Button(root, text='Изменить контакт')
+numbers=Listbox(root, width=40, height=50, selectmode=SINGLE)
 
+
+def info_from_entrys():
+    a=pole_name.get()
+    b=pole_surname.get()
+    c=pole_number.get()
+    info_for_list=a+' '+c+' - '+b
+    numbers.insert(0,info_for_list)
+    
+
+
+add_button =Button(root, text='Добавить контакт',background='green',command=info_from_entrys)
+del_button =Button(root, text='Удалить контакт', background='red')
+edit_button =Button(root, text='Изменить контакт', background='orange')
 
 
 pole_name =Entry(width=20)
@@ -14,9 +26,24 @@ pole_surname =Entry(width=20)
 pole_number =Entry(width=20)
 
 
+
+
 label_name =Label(text='Введите имя')
 label_surname =Label(text='Введите фамилию ')
-label_namber =Label(text='Введите номер')
+label_number =Label(text='Введите номер')
 
+label_name.grid(row=0, column=0)
+label_surname.grid(row=1, column=0)
+label_number.grid(row=2,column=0)
 
-grid.
+pole_name.grid(row=0,column=1)
+pole_number.grid(row=1, column=1)
+pole_surname.grid(row=2,column=1)
+
+numbers.grid(row=10,column=0,)
+
+add_button.grid(row=3,column=1,pady=18)
+del_button.grid(row=9, column=1)
+edit_button.grid(row=9,column=0)
+
+root.mainloop()
